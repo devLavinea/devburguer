2bda992c66bac412696fbc37a5423124afd2d4f0
devLavinea <lavinea2411@gmail.com>
2025-03-27 01:12:56 -0300

    versão final burguer

--- src/javascripts/script.js
+++ src/javascripts/script.js
@@ -190,9 +190,9 @@ const spanItem = document.getElementById("date-span")
 const isOpen = checkRestaurantOpen();
  
 if(isOpen){ 
-    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.remove("bg-red-600");
     spanItem.classList.add("bg-green-600"); 
 }else{ 
     spanItem.classList.remove("bg-green-600") 
-    spanItem.classList.add("bg-red-500")
+    spanItem.classList.add("bg-red-600")
 }
